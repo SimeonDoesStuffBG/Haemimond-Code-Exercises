8d338ddaa018a06ffc47f16cc1de91cd1bdf2017
Simeon Milev <smilev@haemimont.com>
2025-12-05 13:30:32 +0200

    Connected Database

---
+++ Technical_Request/Data/TechnicalRequestContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Technical_Request.Data
+{
+    public class TechnicalRequestContext:DbContext
+    {
+        public TechnicalRequestContext(DbContextOptions<TechnicalRequestContext> options): base(options) { }
+    }
+}
